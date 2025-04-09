dfbaaf54bdf52d77adaba081f613246f332029e1
toushei <87997289+toushei@users.noreply.github.com>
2025-04-09 14:01:56 -0400

    firefighter pin mode

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "frontdist.h"
 #include "motors.h"
 #include "pidstraight.h"
+#include "firefighter.h"
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,15 +12,35 @@ void setup() {
   Wire.begin();
   Wire.setClock(400000); // use 400 kHz I2C
 
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW); // turns the builtin LED on (active low)
+
   imuSetup();
   tofSetup();
   motorSetup();
-  delay(5000);
+  delay(1000);
   Serial.println("Hello setup!");
-  pidForward(10);
+
+  // Move to center of cell off of back wall alignment
+  pidForward(180);
+
+  // Switch options:  
+  if(isFirefighter()) {
+    Serial.println("Firefighter mode");
+    init_GPIO();
+    firefighterSetup();
+    firefighterLoop();
+  }
+
+
 }
 
 void loop() {
   delay(2000);
   Serial.println("Hello main loop!");
+  Serial.print(encLeft.read());
+  Serial.print(" ");
+  Serial.println(encRight.read());
+
+
 }
