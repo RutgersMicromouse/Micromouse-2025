f51b2c163786d7770e816e495f292048d1793c3f
AFE123x <arunf400@gmail.com>
2025-04-09 21:48:29 -0400

    we the people

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,30 @@
+/*
+This example takes range measurements with the VL53L1X and displays additional
+details (status and signal/ambient rates) for each measurement, which can help
+you determine whether the sensor is operating normally and the reported range is
+valid. The range is in units of mm, and the rates are in units of MCPS (mega
+counts per second).
+*/
+
 #include <Wire.h>
 #include <VL53L1X.h>
-#include <Arduino.h>
 
 VL53L1X sensor;
-int pinOne = 5;
-int pinTwo = 4;
-int16_t distanceOne, distanceTwo;
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(pinOne, INPUT);
-  pinMode(pinTwo, INPUT);
-
+void setup()
+{
+  // while (!Serial) {}
   Serial.begin(115200);
-  sensor.setTimeout(500);
-  Wire.begin();
+  Wire.begin(1,2);
   Wire.setClock(400000); // use 400 kHz I2C
+
+  sensor.setTimeout(500);
   if (!sensor.init())
   {
     Serial.println("Failed to detect and initialize sensor!");
     while (1);
   }
-
+  Serial.println("pickle sandwiches");
   // Use long distance mode and allow up to 50000 us (50 ms) for a measurement.
   // You can change these settings to adjust the performance of the sensor, but
   // the minimum timing budget is 20 ms for short distance mode and 33 ms for
@@ -34,28 +37,20 @@ void setup() {
   // inter-measurement period). This period should be at least as long as the
   // timing budget.
   sensor.startContinuous(50);
-  // Serial.println("Hello!");
-
 }
 
+void loop()
+{
+  sensor.read();
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  int16_t t1 = pulseIn(pinOne, HIGH);
-  int16_t t2 = pulseIn(pinTwo, HIGH);
-  distanceOne = (t1 - 1000) * 3 / 4;
-  distanceTwo = (t2 - 1000) * 3 / 4;
+  Serial.print("range: ");
+  Serial.print(sensor.ranging_data.range_mm);
+  Serial.print("\tstatus: ");
+  Serial.print(VL53L1X::rangeStatusToString(sensor.ranging_data.range_status));
+  Serial.print("\tpeak signal: ");
+  Serial.print(sensor.ranging_data.peak_signal_count_rate_MCPS);
+  Serial.print("\tambient: ");
+  Serial.print(sensor.ranging_data.ambient_count_rate_MCPS);
 
-  Serial.printf("left: %d, right: %d, center %d\n",distanceOne,distanceTwo,sensor.readRangeContinuousMillimeters());
-  // if(distanceOne < 100) {
-  //   Serial.println("Left Wall Detected"); 
-  // } 
-
-  // if(distanceTwo < 100) {
-  //   Serial.println("Right Wall Detect");
-  // } 
-
-  delay(100);
-
-
+  Serial.println();
 }
