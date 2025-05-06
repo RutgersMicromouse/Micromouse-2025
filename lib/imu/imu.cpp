81b897a02a6da90dfa51cc03ac1dbc25cac9c696
toushei <87997289+toushei@users.noreply.github.com>
2025-05-06 15:08:11 -0400

    set imu to use external crystal

--- lib/imu/imu.cpp
+++ lib/imu/imu.cpp
@@ -4,7 +4,11 @@ Adafruit_BNO055 bno;
 
 void imuSetup() {
   bno = Adafruit_BNO055(55, 0x28);
-  if (!bno.begin()) Serial.print("No BNO055 detected");
+  if (!bno.begin()) { 
+    Serial.println("No BNO055 detected");
+    while(1);
+  }
+  bno.setExtCrystalUse(true);
 }
 
 double angle() {
